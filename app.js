c1b2857672f8d9b8de8932603c07957154a1465b
someTrickyCase <snowinboots18@gmail.com>
2024-09-26 03:46:42 +0400

    dev

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ const getCategories = require("./scrapper/scrapper");
 const app = express();
 
 async function getData() {
-  const data = await getCategories();
-  console.log(data);
-  return data;
+  console.log("scrapping data...");
+  try {
+    const data = await getCategories();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
 getData();
+
+app.use("/get-categories", (req, res, next) => {
+  console.log("some call in some-categories endpoint");
+  next();
+});
 
 app.get("/get-categories", (req, res) => {
   res.send(data);
 });
 
 app.listen(3000);
+console.log("app started on port 3000");
